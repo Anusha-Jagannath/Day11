0bc08d551527834adbc2861fe96a67403c260808
Anusha-Jagannath <anushajaganath6@gmail.com>
2021-09-04 09:13:52 +0530

    code for managing wage of multiple companies

---
+++ src/com/employee/Company.java
@@ -0,0 +1,61 @@
+package com.employee;
+
+public class Company {
+	private String company;
+	private int wagePerHour;
+	private int noOfWorkingDays;
+	private int maxHourPerMonth;
+	private int totalWage;
+	
+	public Company(String company, int wagePerHour, int noOfWorkingDays, int maxHourPerMonth) {
+		this.company = company;
+		this.wagePerHour = wagePerHour;
+		this.noOfWorkingDays = noOfWorkingDays;
+		this.maxHourPerMonth = maxHourPerMonth;
+	}
+
+	public String getCompany() {
+		return company;
+	}
+
+	public void setCompany(String company) {
+		this.company = company;
+	}
+
+	public int getWagePerHour() {
+		return wagePerHour;
+	}
+
+	public void setWagePerHour(int wagePerHour) {
+		this.wagePerHour = wagePerHour;
+	}
+
+	public int getNoOfWorkingDays() {
+		return noOfWorkingDays;
+	}
+
+	public void setNoOfWorkingDays(int noOfWorkingDays) {
+		this.noOfWorkingDays = noOfWorkingDays;
+	}
+
+	public int getMaxHourPerMonth() {
+		return maxHourPerMonth;
+	}
+
+	public void setMaxHourPerMonth(int maxHourPerMonth) {
+		this.maxHourPerMonth = maxHourPerMonth;
+	}
+
+	public int getTotalWage() {
+		return totalWage;
+	}
+
+	public void setTotalWage(int totalWage) {
+		this.totalWage = totalWage;
+	}
+	
+	
+	
+	
+
+}
